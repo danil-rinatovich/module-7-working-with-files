6a3f7b0bdc9e8f3b8008dc2fea9971bf475e33bd
danil_rinatovich <danil_rinatovich@list.ru>
2024-11-21 15:42:56 +0500

    changet path the files

--- module_7_5.py
+++ module_7_5.py
@@ -5,11 +5,11 @@ directory = os.getcwd()
 
 for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(directory)
-    filetime = os.path.getmtime(directory)
+    filepath = os.path.join(root, file)
+    filetime = os.path.getmtime(filepath)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    filesize = os.path.getsize(directory)
-    parent_dir = os.path.dirname(directory)
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
     print(f'Обнаружен файл: {file}, Путь: {filepath}, '
           f'Размер: {filesize} байт, Время изменения: {formatted_time}, '
           f'Родительская директория: {parent_dir}')
